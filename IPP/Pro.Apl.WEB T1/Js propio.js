84812df6bf7f59a4b01147baab319fd57851b043
kinapam <138642034+kinapam@users.noreply.github.com>
2023-07-08 16:10:49 -0400

    Finalizado

---
+++ IPP/Pro.Apl.WEB T1/Js propio.js
@@ -0,0 +1,6 @@
+alert('Bienvenido a ∃xist el futuro lo espera')
+var user1;
+user1= prompt ('Ingrese su nombre de usuario','tu nombre');
+var mensaje = 'Que tengas una linda experiencia ';
+mensaje = mensaje.concat(user1)
+alert(mensaje)
