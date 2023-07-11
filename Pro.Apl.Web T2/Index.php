8e80c4c50f10f6c9c164426dc8de7ce874b06ba8
kinapam <138642034+kinapam@users.noreply.github.com>
2023-07-11 02:28:15 -0400

    Configuración PHP

---
+++ Pro.Apl.Web T2/Index.php
@@ -0,0 +1,10 @@
+<?php
+    /*comentarios*/
+    // comentario individual
+    // print = echo
+    // input = fgets
+
+    echo "hola\n";
+    $tex1 = "Favor ingresa tu nombre";
+    $nombre = fgets(STDIN);
+    $apellido = fgets(STDIN);
